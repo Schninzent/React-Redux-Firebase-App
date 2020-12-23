1845fb57cc8f002e70ce978a694c0b53a881c0b4
Schninzent <vweber@stud.fra-uas.de>
2020-12-23 16:33:35 +0100

    comments

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -7,12 +7,14 @@ class Dashboard extends Component {
   render() {
     
     // console.log(this.props);
+    //holt das projects property aus den props direkt raus
     const { projects } = this.props;
     
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
+            //übergibt das projects property an die ProjectsList Funktion
             <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
@@ -24,10 +26,13 @@ class Dashboard extends Component {
   }
 }
 
+//auf state von store zugreifen
 const mapStateToProps = (state) => {
   return {
+    //neues Objekt erstellen um die Projekte aus dem Store zu speichern
+    //Dieses Objekt representiert die properties welche an die props der Dashboard component zugefügt wurden
     projects: state.project.projects
   }
 }
-
+//Store mit Dashboard verbinden -- mapStateToProps in connect geben damit es weiß welche Daten geholt werden müssen
 export default connect(mapStateToProps)(Dashboard)
--- marioplan/src/components/projects/ProjectList.js
+++ marioplan/src/components/projects/ProjectList.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
 
+
+//------------------>nimmt das projects property direkt aus den props welche übergeben werden
 const ProjectList = ({projects}) => {
   return (
     <div className="project-list section">
+      //nur wenn projects Inhalt hat, dann führe .map aus 
       { projects && projects.map(project => {
         return (
+          //project und id prop an Summary übergeben
           <ProjectSummary project={project} key={project.id} />
         )
       })}  
